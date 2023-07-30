71421bf4fa38f5bec037dd81a57c7218a6275df5
Alexey Kasyanik <alekseykasuanik@gmail.com>
2023-07-30 13:28:35 +0300

    Initial Commit

---
+++ CollageBuilder/CollageBuilderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CollageBuilderApp.swift
+//  CollageBuilder
+//
+//  Created by Алексей Касьяник on 30.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct CollageBuilderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
